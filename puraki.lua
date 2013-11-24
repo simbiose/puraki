30ccdfff32522aa146cdeff37412c85c820669a9
leite <xxleite@gmail.com>
2013-11-24 02:20:59 -0200

    change api, better approach

--- puraki.lua
+++ puraki.lua
@@ -5,200 +5,229 @@
 -- this stuff is worth it, you can buy me a beer in return
 -- ----------------------------------------------------------------------------
 
-local ta, co, ti, os = 
-  require('table'), require('coroutine'), require('timer'), require('os')
+local co, os, st = require('coroutine'), require('os'), require('string')
+local _, ffi = pcall(require, 'ffi')
 
-local puraki, set_timeout, clear_timer, set_interval, create, yield,
-      resume, status, insert, remove, time = 
-      {}, ti.setTimeout, ti.clearTimer, ti.setInterval, co.create, co.yield,
-      co.resume, co.status, ta.insert, ta.remove, os.time
+local create, yield, resume, status, time, execute, format =
+      co.create, co.yield, co.resume, co.status, os.time, os.execute, st.format
 
-co, ta, ti, os = nil, nil, nil, nil
+local sleep
+if _ then
+  ffi.cdef[[void Sleep(int ms);
+  int poll(struct pollfd *fds, unsigned long nfds, int timeout);]]
 
-function puraki.new()
-  -- body
-
-  local this, contexts, tasks, coroutines, scope, stop, finish, loop =
-    {}, {}, {}, {}, function() return end, true, true, nil
-
-  local function continue(index)
-    if (coroutines[index] and status(coroutines[index]) == 'suspended') and 
-      not stop then
-      resume(coroutines[index])
-    end
+  if ffi.os == 'Windows' then
+    function sleep(s) ffi.C.Sleep((s * 1000)) end
+  else
+    function sleep(s) ffi.C.poll(nil, 0, (s * 1000)) end
   end
+else
+  function sleep(s) execute('sleep ' .. tonumber(s)) end
+end
 
-  -- create task also recycle
-  local function bootstrap_task(index)
-    local context = {
-        max_idle_time=60, ttl=0, time=0, task=0,
-        cicles=1, keep=false, status='stoped'
+co, os, st = nil, nil, nil
+
+local puraki, routine = {}, {}
+
+local function Routine(task, index, env)
+  local this = {
+    task      = nil,
+    coroutine = nil,
+    time      = 0,
+    idle_time = 0,
+    status    = 1, -- 0 running, 1 stopped, 2 waiting (to resume), 3 error
+    cause     = '',
+    context   = {
+        idle_time = 60,
+        ttl       = 0,
+        task      = index,
+        cicles    = 1,
+        boots     = 1,
+        keep      = false,
+        interval  = -1
       }
-    local task = scope(this, context)
-
-    if 'function' ~= type(task) then return false end
-
-    contexts[index] = (contexts[index] or context)
-    task[index]     = task
-
-    return true
-  end
-
-  -- create a loop/sentinel thru tasks
-  local function tasks_loop()
-
-    -- check loop
-    if loop then
-      return
-    end
-
-    -- interval
-    loop = set_interval((this.cicle or 500), function()
-
-      local i = 1
-      for i = 1, this.parallel do
-
-        if #coroutines == 0 and finish then
-          clear_timer(loop)
-          loop = nil
-        end
-
-        if coroutines[i] then
-          if status(coroutines[i]) == 'dead' then
-            if finish or (contexts[i] and contexts[i].keep) then
-              coroutines[i] = nil
-              if (contexts[i] and contexts[i].keep) then
-                bootstrap_task(i)
-              else
-                i = i - 1
-              end
-            end
-          elseif status(coroutines[i]) == 'suspended' and not stop then
-            resume(coroutines[i])
-          end
-        else
-          if not stop and not finish then
-            create_task(i)
-          end
-        end
-      end
-    end)
-  end
-
-  -- create and run task
-  create_task = function(index)
-    local coroutine
-    coroutines[index] = coroutine
-
-    -- populate metadata
-    contexts[index].time   = time()
-    contexts[index].status = 'running'
-    contexts[index].task   = index
-    if contexts[index].ttl > 0 then
-      contexts[index].ttl = (contexts[index].time + contexts[index].ttl)
-    end
-
-    local function manager()
-
-      local err, cause = true, ''
-
-      -- task loop
-      while true do
-        contexts[index].idle_time = (time() + contexts[index].max_idle_time)
-
-        err, cause = pcall(tasks[index], contexts[index])
-
-        if not err then
-          contexts[index].error  = cause
-          contexts[index].status = 'error'
-        else
-          if (contexts[index].ttl > 0 and contexts[index].ttl < time()) or 
-            cause == true then
-            contexts[index].status = 'stop'
-          end
-        end
-
-        if finish or contexts[index].status ~= 'running' then
-          return true
-        else
-          set_timeout(this.interval, continue, index)
-        end
-
-        yield()
-
-        contexts[index].cicle  = contexts[index].cicle + 1
-      end
-    end
-
-    coroutines[index] = create(manager)
-    resume(coroutines[index])
-  end
-
-  this = {
-
-    cicle    = 500,
-    interval = 400,
-    parallel = 20,
-
-    -- create a task
-    task = function(self, callback_scope)
-      scope = callback_scope
-      if #tasks == this.parallel then return end
-
-      local i = 0
-      for i = (#tasks == 0 and 1 or #tasks), self.parallel do
-        if not bootstrap_task(i) then break end
-      end
-    end,
-
-    -- resume queue
-    resume = function(self)
-      stop = false
-    end,
-
-    -- stop queue
-    stop = function(self)
-      stop = true
-    end,
-
-    -- run tasks
-    run = function(self)
-      local i = 1
-      stop, finish = false, false
-      for i = 1, #tasks do
-        create_task(i)
-      end
-      tasks_loop()
-    end,
-
-    -- clear tasks queue
-    clear_queue = function(self, ...)
-      local options = {...}
-      local i, force, callback = 1, (options[1] and options[1] or false), 
-                                    (options[2] and options[2] or function() end)
-
-      stop, finish = true, true
-      if force then
-        for i=1, #coroutines do
-          coroutines[i] = nil
-          remove(coroutines, i)
-          i = i - 1
-        end
-        callback()
-      else
-        local cq 
-        cq = set_interval((self.cicle || 500), function()
-          if #coroutines == 0 then
-            clear_timer(cq)
-            callback()
-          end
-        end)
-      end
-    end
   }
 
-  setmetatable(this, puraki)
+  setmetatable(this, {__index = routine})
+  this:bootstrap(task, false)
   return this
 end
 
-return puraki
+function routine:continue()
+  if (self.coroutine and status(self.coroutine) == 'suspended') then
+    self.status = 0
+    resume(self.coroutine, self)
+  end
+end
+
+function routine:keep()
+  return (self.context.keep)
+end
+
+function routine:dead()
+  return (not self.coroutine or status(self.coroutine) == 'dead')
+end
+
+function routine:stopped()
+  return (self.status == 1)
+end
+
+function routine:running()
+  return (self.status == 0)
+end
+
+function routine:waiting()
+  return (self.status == 2)
+end
+
+function routine:error()
+  return (self.status == 3)
+end
+
+function routine:debug()
+  print(self.status, status(self.coroutine), self:idle(), self:expired())
+end
+
+function routine:expired()
+  return (self.context.ttl > 0 and self.context.ttl < time())
+end
+
+function routine:idle()
+  return (self.idle_time < time())
+end
+
+function routine:manager()
+  local not_err, cause
+
+  local function continue()
+    self:continue()
+  end
+
+  while true do
+    self.idle_time = (time() + self.context.idle_time)
+    self.status    = 0
+
+    not_err, cause = pcall(self.task, self.context, continue)
+
+    if not not_err then
+      self.status = 3
+      self.cause  = format("%d: %s", time(), tostring(cause))
+    end
+
+    if self:expired() or self:error() or (not_err and cause == false) then
+      self.status = 1
+      return true
+    elseif not_err and cause == true then
+      self.status = 2
+    end
+
+    yield()
+
+    self.context.cicles = (self.context.cicles + 1)
+  end
+end
+
+function routine:bootstrap(scope, run_it)
+  if scope then
+    local task = scope(self.context)
+
+    if 'function' ~= type(task) then
+      return false
+    else
+      self.task = task
+    end
+  end
+
+  self.context.time   = time()
+  self.context.status = 0
+  self.context.boots  = (self.context.boots + 1)
+  if self.context.ttl > 0 then
+    self.context.ttl = (self.context.time + self.context.ttl)
+  end
+
+  self.coroutine = create(self.manager)
+  if run_it then
+    resume(self.coroutine, self)
+  end
+
+  return true
+end
+
+
+--[[
+
+]]
+
+-- create a loop/sentinel
+local function loop(tasks)
+  local all_dead = true
+  local index, routine
+
+  while true do
+    all_dead = true
+
+    for index, routine in ipairs(tasks) do
+      if routine:dead() then
+        if routine:keep() then
+          all_dead = false
+          routine:bootstrap(nil, true)
+        end
+      elseif routine:idle() then
+        -- kill!
+      elseif routine:waiting() and not routine:expired() then
+        all_dead = false
+        routine:continue()
+      else
+        all_dead = false
+      end
+    end
+
+    sleep(0.01)
+
+    if all_dead then
+      return
+    end
+  end
+end
+
+
+-- create a task
+function puraki:task(scope, env)
+  if #self.tasks == self.spawn then return end
+  local i, x = 0, 0
+  for i = (#self.tasks == 0 and 1 or #self.tasks), self.spawn do
+    if 'table' == type(scope) then
+      for x = 1, #scope do
+        self.tasks[(#self.tasks + 1)] = Routine(scope, (#self.tasks + 1))
+      end
+    else
+      self.tasks[(#self.tasks + 1)] = Routine(scope, (#self.tasks + 1))
+    end
+  end
+end
+
+-- run tasks
+function puraki:run()
+  local index, routine
+  for index, routine in ipairs(self.tasks) do
+    routine:bootstrap(nil, true)
+  end
+  loop(self.tasks)
+end
+
+-- clear tasks queue
+function puraki:clear()
+  local i = 0
+  for i = 1, #tasks do
+    tasks[i] = nil
+  end
+  tasks = nil
+end
+
+local function Puraki()
+  local this = {spawn = 1, tasks = {}}
+  setmetatable(this, {__index = puraki})
+  return this
+end
+
+return Puraki
